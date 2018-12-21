2d7a1fd8dc6f44c297c985920b6668d58b86dff6
michaelzhou1987 <39017211+michaelzhou1987@users.noreply.github.com>
2018-12-21 21:38:37 +0800

    Update PinkiePromise.js

---
+++ modules/PinkiePromise.js
@@ -0,0 +1,78 @@
+class PinkiePromise {
+    constructor(callback) {
+        this._status = 'pending';
+        this._kisses = null;
+        this._farts = null;
+
+        this._unicorns = [];
+        this._monsters = [];
+
+        const resolve = kisses => {
+            this._status = 'fulfill';
+            this._kisses = kisses;
+
+            startTasks();
+        }
+
+        const reject = farts => {
+            this._status = 'reject';
+            this._farts = farts;
+
+            startTasks();
+        }
+
+
+        
+        callback(resolve, reject);
+    }
+
+    then(resolve, reject) {
+        if(this._status === 'pending') {
+            this._unicorns.push(resolve);
+            this._monsters.push(reject);
+        } else if (this._status === 'fulfill') {
+            resolve(this._kisses);
+        } else {
+            reject(this._farts);
+        }
+
+        return this;
+    }
+
+    done(callback) {
+        if(this._status === 'pending') {
+            this._unicorns.push(callback);
+        } else if(this._status === 'fulfill') {
+            callback(this._kisses);
+        } else {
+            //do nothing...
+        }
+        return this;
+    }
+
+    catch(callback) {
+        if(this._status === 'reject') {
+            this._monsters.push(callback);
+        } else if(this._status === 'reject') {
+            callback(this._farts);
+        } else {
+            //do nothing...
+        }
+        return this;
+    }
+
+    startTasks() {
+        if(this._status === 'fulfill') {
+            for(let i=0; i<this._unicorns.length; i++) {
+                this._unicorns[i](this._kisses);
+            }
+            this._unicorns = [];
+        } else if(this._status === 'reject') {
+            for (let i = 0; i < this._monsters.length; i++) {
+                this._monsters[i](this._farts);
+            }
+            this._monsters = [];
+        }
+        return this;
+    }
+}
